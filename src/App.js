ef8946354af33e9c8c9130513f014c9aa570db78
dietrich <dietrich@newskylabs.net>
2019-11-25 19:21:18 +0100

    Configured React's production build to serve the site from the subdirectory '/hanzi-reader'
    by
    
      - Adding "homepage": "/hanzi-reader" to package.json
      - Adding a router with basename='/hanzi-reader' to src/App.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,73 @@
+/**
+  Hanzi Reader main file.
+ 
+  Main file of Hanzi Reader.
+ 
+  @file       App.js
+ 
+  @author     Dietrich Bollmann
+  @email      dietrich@formgames.org
+  @copyright  Copyright 2018 Dietrich Bollmann
+  @license    Apache License 2.0, http://www.apache.org/licenses/LICENSE-2.0
+  @date       2018/02/25
+ */
+
 import React from 'react';
-// TODO: Create and integrate a logo:
-//| import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <h1>
-      Hello Hanzi Reader
-    </h1>
+    <Router basename='/hanzi-reader'>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/users'>Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/users'>
+            <Users />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h1>Hanzi Reader Home</h1>;
+}
+
+function About() {
+  return <h1>Hanzi Reader About</h1>;
+}
+
+function Users() {
+  return <h1>Hanzi Reader Users</h1>;
+}
+
+/* fin */
