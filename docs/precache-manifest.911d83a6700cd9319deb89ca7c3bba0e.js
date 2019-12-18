8d2b20763744ad28eab26834c5a5cb05e0fbc892
dietrich <dietrich@newskylabs.net>
2019-12-18 19:45:29 +0100

    Deploying Hanzi Reader to github

---
+++ docs/precache-manifest.911d83a6700cd9319deb89ca7c3bba0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5c1f9db865eb36d5d40a98136e0f04",
+    "url": "/aireader/index.html"
+  },
+  {
+    "revision": "829e0de72bfae3737c3a",
+    "url": "/aireader/static/css/main.47250da5.chunk.css"
+  },
+  {
+    "revision": "f856c41d2070dc3fc41a",
+    "url": "/aireader/static/js/2.59620a69.chunk.js"
+  },
+  {
+    "revision": "829e0de72bfae3737c3a",
+    "url": "/aireader/static/js/main.98b8c58e.chunk.js"
+  },
+  {
+    "revision": "0aa6d6b33113cca1c240",
+    "url": "/aireader/static/js/runtime-main.8066c775.js"
+  }
+]);
